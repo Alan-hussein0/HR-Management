8fd1732164444fe41cbfb8594535d5ea8ca22628
alan <swar.jan21@gmail.com>
2023-03-31 15:15:31 +0300

    convert the type column to enum

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             // $table->string('name');
-            $table->string('type')->default('employee');
+            $table->enum('type',['fonder','employee'])->default('employee');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
