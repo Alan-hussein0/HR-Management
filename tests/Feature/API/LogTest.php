7326fa180f5f6d512ff98f64b1e0a641592bb8e5
alan <swar.jan21@gmail.com>
2023-04-10 22:25:31 +0300

    add some test function to logsTest

--- tests/Feature/API/LogTest.php
+++ tests/Feature/API/LogTest.php
@@ -68,4 +68,37 @@ class LogTest extends TestCase
 
         $this->assertDatabaseHas('logs',['title'=>'test']);
     }
+
+    public function test_show_log_with_specific_date()
+    {
+        $log = Log::factory(3)->create();
+
+        $response = $this->getJson('api/employees/'.$log->first()->created_at.'/logs',$this->header(user: $this->founder));
+
+        $response->assertStatus(200)
+                ->assertJsonFragment([
+                    'success' => true,
+                    'message' => 'all logs of the date have been retrieved successfully!'
+                ]
+            );
+
+        //it must have 4 because when we call the request it is add new log record, so 3 by factory and 1 when call api
+        $this->assertDatabaseCount('logs',4);
+    }
+
+    public function test_show_log_with_specific_date_unauthorized_return_error()
+    {
+        $log = Log::factory(3)->create();
+
+        $response = $this->getJson('api/employees/'.$log->first()->created_at.'/logs',$this->header(user: $this->createEmployee(jobTitle: 'it',user_id: $this->createUser(type: 'employee')->id)->user));
+
+        $response->assertStatus(403)
+                ->assertJsonFragment([
+                    'message' => 'you not authrized for this process'
+                ]
+            );
+
+        //it must have 3 because when we call the request it will fail so now log record has been added.
+        $this->assertDatabaseCount('logs',3);
+    }
 }
